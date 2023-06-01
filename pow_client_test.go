47dd094b88b64f4a8392636115225deb6d479707
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-06-01 14:23:42 +0800

    git init

---
+++ pow_client_test.go
@@ -0,0 +1,39 @@
+package powclient
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/url"
+	"os"
+	"testing"
+)
+
+func TestGetToken(t *testing.T) {
+	token, err := getToken("103.120.18.35", "api.leo.moe", "443")
+	fmt.Println(token, err)
+	assert.NoError(t, err, "GetToken() returned an error")
+}
+
+func getToken(fastIp string, host string, port string) (string, error) {
+	var baseURL = "/v3/challenge"
+	getTokenParams := NewGetTokenParams()
+	u := url.URL{Scheme: "https", Host: fastIp + ":" + port, Path: baseURL}
+	getTokenParams.BaseUrl = u.String()
+	getTokenParams.SNI = host
+	getTokenParams.Host = host
+	var err error
+	// 尝试三次RetToken，如果都失败了，异常退出
+	for i := 0; i < 3; i++ {
+		token, err := RetToken(getTokenParams)
+		if err != nil {
+			continue
+		}
+		return token, nil
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("RetToken failed 3 times, exit")
+	os.Exit(1)
+	return "", nil
+}
