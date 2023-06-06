61b7d576afceb68895d63c55537da50a90e273f2
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-06-07 02:17:37 +0800

    增加代理支持

--- pow_client.go
+++ pow_client.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"math/big"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -41,6 +42,7 @@ type GetTokenParams struct {
 	UserAgent   string
 	SNI         string
 	Host        string
+	Proxy       string /** 支持socks5:// http:// **/
 }
 
 func NewGetTokenParams() *GetTokenParams {
@@ -52,27 +54,20 @@ func NewGetTokenParams() *GetTokenParams {
 		UserAgent:   "POW client",
 		SNI:         "",
 		Host:        "",
+		Proxy:       "",
 	}
+}
+
+type ChallengeParams struct {
+	BaseUrl     string
+	RequestPath string
+	SubmitPath  string
+	UserAgent   string
+	Host        string
+	Client      *http.Client
 }
 
 func RetToken(getTokenParams *GetTokenParams) (string, error) {
-	// Get challenge
-	challengeResponse, err := requestChallenge(getTokenParams)
-	if err != nil {
-		return "", err
-	}
-	//fmt.Println(challengeResponse.Challenge.Challenge)
-
-	// Solve challenge and submit answer
-	token, err := submitAnswer(getTokenParams, challengeResponse)
-	if err != nil {
-		return "", err
-	}
-
-	return token, nil
-}
-
-func requestChallenge(getTokenParams *GetTokenParams) (*RequestResponse, error) {
 	client := &http.Client{
 		Timeout: getTokenParams.TimeoutSec,
 	}
@@ -85,14 +80,43 @@ func requestChallenge(getTokenParams *GetTokenParams) (*RequestResponse, error) 
 			},
 		}
 	}
-	req, err := http.NewRequest("GET", getTokenParams.BaseUrl+getTokenParams.RequestPath, nil)
+	if getTokenParams.Proxy != "" {
+		proxyURL, _ := url.Parse(getTokenParams.Proxy)
+		client.Transport.(*http.Transport).Proxy = http.ProxyURL(proxyURL)
+	}
+	challengeParams := &ChallengeParams{
+		BaseUrl:     getTokenParams.BaseUrl,
+		RequestPath: getTokenParams.RequestPath,
+		SubmitPath:  getTokenParams.SubmitPath,
+		UserAgent:   getTokenParams.UserAgent,
+		Host:        getTokenParams.Host,
+		Client:      client,
+	}
+	// Get challenge
+	challengeResponse, err := requestChallenge(challengeParams)
+	if err != nil {
+		return "", err
+	}
+	//fmt.Println(challengeResponse.Challenge.Challenge)
+
+	// Solve challenge and submit answer
+	token, err := submitAnswer(challengeParams, challengeResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func requestChallenge(challengeParams *ChallengeParams) (*RequestResponse, error) {
+	req, err := http.NewRequest("GET", challengeParams.BaseUrl+challengeParams.RequestPath, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("User-Agent", getTokenParams.UserAgent)
+	req.Header.Add("User-Agent", challengeParams.UserAgent)
 	//req.Header.Add("Host", getTokenParams.Host)
-	req.Host = getTokenParams.Host
-	resp, err := client.Do(req)
+	req.Host = challengeParams.Host
+	resp, err := challengeParams.Client.Do(req)
 	if err != nil || resp.StatusCode != http.StatusOK {
 		// 如果http_code为429
 		if resp.StatusCode == http.StatusTooManyRequests {
@@ -115,19 +139,7 @@ func requestChallenge(getTokenParams *GetTokenParams) (*RequestResponse, error) 
 	return &challengeResponse, nil
 }
 
-func submitAnswer(getTokenParams *GetTokenParams, challengeResponse *RequestResponse) (string, error) {
-	client := &http.Client{
-		Timeout: getTokenParams.TimeoutSec,
-	}
-	if getTokenParams.SNI != "" {
-		client = &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					ServerName: getTokenParams.SNI,
-				},
-			},
-		}
-	}
+func submitAnswer(challengeParams *ChallengeParams, challengeResponse *RequestResponse) (string, error) {
 	requestTime := challengeResponse.RequestTime
 	challenge := challengeResponse.Challenge.Challenge
 	requestId := challengeResponse.Challenge.RequestID
@@ -152,16 +164,16 @@ func submitAnswer(getTokenParams *GetTokenParams, challengeResponse *RequestResp
 		return "", err
 	}
 
-	req, err := http.NewRequest("POST", getTokenParams.BaseUrl+getTokenParams.SubmitPath, bytes.NewBuffer(requestBody))
+	req, err := http.NewRequest("POST", challengeParams.BaseUrl+challengeParams.SubmitPath, bytes.NewBuffer(requestBody))
 	if err != nil {
 		return "", err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Add("User-Agent", getTokenParams.UserAgent)
+	req.Header.Add("User-Agent", challengeParams.UserAgent)
 	//req.Header.Add("Host", getTokenParams.Host)
-	req.Host = getTokenParams.Host
+	req.Host = challengeParams.Host
 
-	resp, err := client.Do(req)
+	resp, err := challengeParams.Client.Do(req)
 	if err != nil {
 		return "", err
 	}
